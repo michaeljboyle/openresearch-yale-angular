9270e6c57ec5d21094e97ff47c37d2d7e2de26e4
Michael Boyle <michael.jarvis.boyle@gmail.com>
2018-01-06 23:38:01 -0800

    Reorganize and add gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,59 @@
+/* eslint-env node */
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+var jasmine = require('gulp-jasmine-phantom');
+var eslint = require('gulp-eslint');
 
-gulp.task('default', defaultTask);
+gulp.task('default', serve);
 gulp.task('styles', convertSass);
+gulp.task('tests', tests);
+gulp.task('lint', lint);
 
-function defaultTask(done) {
-  gulp.watch('assets/sass/**/*.scss', convertSass);
-  done();
+function serve(done) {
+  browserSync.init({
+    server: './src/client',
+  });
+  gulp.watch('src/client/assets/sass/**/*.scss', convertSass);
+  gulp.watch('src/client/**/*.js', lint);
+  gulp.watch('src/client/**/*.html').on('change', browserSync.reload);
 }
 
 function convertSass(done) {
-    gulp.src('assets/sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions']
-        }))
-        .pipe(gulp.dest('assets/css/'));
-    done();
+  gulp.src('src/client/assets/sass/**/*.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions'],
+  }))
+  .pipe(gulp.dest('src/client/assets/css/'))
+  .pipe(browserSync.stream());
+  done();
+}
+
+function tests(done) {
+  gulp.src('tests/*.js')
+  .pipe(jasmine({
+    integration: true,
+    vendor: 'src/client/**/*.js',
+  }));
+  done();
+}
+
+function lint(done) {
+  // ESLint ignores files with "node_modules" paths.
+  // So, it's best to have gulp ignore the directory as well.
+  // Also, Be sure to return the stream from the task;
+  // Otherwise, the task may end before the stream has finished.
+  gulp.src(['**/*.js', '!node_modules/**'])
+    // eslint() attaches the lint output to the "eslint" property
+    // of the file object so it can be used by other modules.
+    .pipe(eslint())
+    // eslint.format() outputs the lint results to the console.
+    // Alternatively use eslint.formatEach() (see Docs).
+    .pipe(eslint.format())
+    // To have the process exit with an error code (1) on
+    // lint error, return the stream and pipe to failAfterError last.
+    .pipe(eslint.failAfterError());
 }
