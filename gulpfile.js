936f7e5e6b7aa6369f5f851b108db9793a672e27
Michael Boyle <michael.jarvis.boyle@gmail.com>
2018-01-07 17:10:00 -0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -57,4 +57,5 @@ function lint(done) {
     // To have the process exit with an error code (1) on
     // lint error, return the stream and pipe to failAfterError last.
     .pipe(eslint.failAfterError());
+  done();
 }
